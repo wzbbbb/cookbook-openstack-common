6026fa82693ccd1a56632466abd6f57f71119000
ZHI BING WANG <wzbbbb@yahoo.com>
2015-04-30 16:56:49 -0700

    modified:   default-overrides.rb

--- recipes/default-overrides.rb
+++ recipes/default-overrides.rb
@@ -4,7 +4,7 @@
 # Prod Env analogs
 # Networking: 
 #  - Ports: eth1 -> bond1.2001 (LCP)
-#  -        eth2 -> bond1.2004 (tunnel interface)
+#  -        bond0 -> bond1.2004 (tunnel interface)
 #  -        eth3 -> bond1.2002 (Data Plane!!)
 
 
@@ -31,16 +31,16 @@ node.override["openstack"]["network"]["ml2"]["integration_bridge"] = "br-int"
 node.override["openstack"]["network"]["ml2"]["tunnel_bridge"] = "br-tun"
 node.override["openstack"]["network"]["ml2"]["enable_tunneling"] = "True"
 node.override["openstack"]["network"]["ml2"]["fw_driver"] = "neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver"
-node.override["openstack"]["network"]["ml2"]["local_ip_interface"] = "eth2"
+node.override["openstack"]["network"]["ml2"]["local_ip_interface"] = "bond0"
 #node.override['openstack']['network']['openvswitch']['local_ip_interface'] = "eth1" 
 # L3 external bridge binding
 node.override["openstack"]["network"]["l3"]["external_network_bridge_interface"] = "bond0"
 # Openvswitch 
-node.override["openstack"]["network"]["openvswitch"]["local_ip_interface"] = "eth2"
+node.override["openstack"]["network"]["openvswitch"]["local_ip_interface"] = "bond0"
 node.override["openstack"]["network"]["openvswitch"]["tunnel_id_ranges"] = "1:32000"
 node.override["openstack"]["network"]["openvswitch"]["enable_tunneling"] = "True"
 node.override["openstack"]["network"]["openvswitch"]["tenant_network_type"] = "gre"
 # Nova network type (agent)
 node.override["openstack"]["compute"]["network"]["service_type"] = "neutron"
 # Vagrant / virtual box - related changes
-node.override["openstack"]["compute"]["libvirt"]["virt_type"] = "qemu"
+node.override["openstack"]["compute"]["libvirt"]["virt_type"] = "kvm"
